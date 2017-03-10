79be11f9c386dcf0b3b82b3199cacbc47adbf426
David Reaburn <davidrea@microsoft.com>
2017-03-10 12:26:57 -0800

    Packaging for GitHub.

---
+++ EndianBitConverter.Tests/InterfaceTests.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (C) Microsoft Corporation. All rights reserved.
+
+namespace BitConverter.Tests
+{
+    using System.Reflection;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class InterfaceTests
+    {
+        [TestMethod]
+        public void IsLittleEndian()
+        {
+            Assert.IsFalse(EndianBitConverter.BigEndian.IsLittleEndian);
+            Assert.IsTrue(EndianBitConverter.LittleEndian.IsLittleEndian);
+        }
+    }
+}
